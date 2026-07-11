c62372d5ae1c6fdeb2aa0d57f13299ca8e15ad35
Panagiotis Dimitriou <dimitrioupanagiotis@outlook.com>
2026-07-11 07:37:31 +0000

    feat(terraform): add vpc module for multi-az networks
    Private and public subnets with NAT gateways and kubernetes subnet
    tags for internal and external load balancers.
    
    Co-authored-by: panagiod <panagiod@users.noreply.github.com>

---
+++ terraform/modules/vpc/main.tf
@@ -0,0 +1,95 @@
+terraform {
+  required_version = ">= 1.5.0"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 5.40"
+    }
+  }
+}
+
+variable "name" {
+  description = "Name prefix for VPC resources."
+  type        = string
+}
+
+variable "cidr" {
+  description = "VPC CIDR block."
+  type        = string
+  default     = "10.0.0.0/16"
+}
+
+variable "azs" {
+  description = "Availability zones."
+  type        = list(string)
+}
+
+variable "private_subnets" {
+  description = "Private subnet CIDRs."
+  type        = list(string)
+}
+
+variable "public_subnets" {
+  description = "Public subnet CIDRs."
+  type        = list(string)
+}
+
+variable "single_nat_gateway" {
+  description = "Use a single NAT gateway (cost saving for staging)."
+  type        = bool
+  default     = false
+}
+
+variable "tags" {
+  description = "Additional tags."
+  type        = map(string)
+  default     = {}
+}
+
+locals {
+  tags = merge(var.tags, {
+    "kubernetes.io/cluster/${var.name}" = "shared"
+  })
+}
+
+module "vpc" {
+  source  = "terraform-aws-modules/vpc/aws"
+  version = "~> 5.5"
+
+  name = var.name
+  cidr = var.cidr
+
+  azs             = var.azs
+  private_subnets = var.private_subnets
+  public_subnets  = var.public_subnets
+
+  enable_nat_gateway = true
+  single_nat_gateway = var.single_nat_gateway
+
+  public_subnet_tags = {
+    "kubernetes.io/role/elb" = "1"
+  }
+
+  private_subnet_tags = {
+    "kubernetes.io/role/internal-elb" = "1"
+  }
+
+  tags = local.tags
+}
+
+output "vpc_id" {
+  value = module.vpc.vpc_id
+}
+
+output "private_subnets" {
+  value = module.vpc.private_subnets
+}
+
+output "public_subnets" {
+  value = module.vpc.public_subnets
+}
+
+output "vpc_cidr_block" {
+  value = module.vpc.vpc_cidr_block
+}
